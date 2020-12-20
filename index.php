7022caec320cd70a18eced61a9e79f8116dbcbd5
Guilherme Grillo <carloseam@gmail.com>
2020-12-19 23:09:54 -0300

    Novo commit

--- index.php
+++ index.php
@@ -26,12 +26,24 @@
                 obs: sempre dar um git add -A antes do commit para salvar as alterações primeiro antes de enviar ao repositorio 
                 5.1 - comando para ver o historio de todos os commits (versoes) feitos do projeto no repositorio:
                 git log
+                5.2 - comando para realizar adicao de novos arquivos e alterações e commits ao mesmo tempo no repositorio. Muito
+                util em situações que não se deseja ter que ficar trackeando cada arquivo novo por vez que for adicionado para 
+                depois comitar: git add - A && git commit -m "Novo commit"
             6 - Branch: um branch representa um repositorio que conterá todos os commits (versoes) do projeto. O Git permite criar
             mais um de um brach para termos mais de um repositorio do projeto, sendo que cada alteração que ocorrer irá corresponder
             ao branch desse projeto em específico.
                 6.1 - mostra todos os branchs do projeto: git branch
                 6.2 - cria um novo branch: git branch "novo_branch"
                 6.3 - muda do branch atual para esse novo branch: git checkout "outro_branch"
+            
+            - INTEGRANDO REPOSITÓRIO LOCAL GIT COM REPOSITÓRIO REMOTO DO GITHUB
+            1 - gerar uma chave SSH para autenticar o computador onde o projeto se encontra com o GitHub:
+            comando: ssh-keygen -t rsa -b 4096 -C "your_email@example.com"
+            onde "your_email@example.com" corresponde ao e-mail da conta do usuario no github
+            2 - após esse comando, ir ao local onde o arquivo (id_rsa.pub) com a chave publica foi gerada, pegar essa chave e registrá-la
+            na conta do github (menu settings, opcao SSH and GPG Keys)
+            3 - criar um novo respositorio no git hub
+            4 - commit remoto no repositorio do github
         -->
 
         <?php
